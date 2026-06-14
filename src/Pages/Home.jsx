3fb75e14b623a855e155f099a1fb507065e7fc2b
ganesh-webdevloper <ganeshwebdevloper@gmail.com>
2026-06-14 21:30:04 +0530

    added emoji in home pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='bg-green-200 h-full font-bold flex items-center justify-center'>Welcome to Home page</div>
+    <div className='bg-green-200 h-full font-bold flex items-center justify-center text-3xl'>Welcome to Home page😎
+      
+      
+    
+    </div>
+    
     
   )
 }
