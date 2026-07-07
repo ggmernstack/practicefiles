4e35d5616ca785d3251aec71e97640928aaa96bc
ggmernstack <ggmernstack@gmail.com>
2026-07-07 23:05:52 +0530

    edited contact pages

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-     <div className='bg-green-200 h-full font-bold flex items-center justify-center'>Contact page</div>
+     <div className='bg-green-200 h-full font-bold flex items-center justify-center'>Hello, this is the Contact page😎</div>
   )
 }
 
